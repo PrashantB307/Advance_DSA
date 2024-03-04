1fceaedd8030099c7cd3efae480433b2ccc07d4a
PrashantB-307 <pb28.rkt@gmail.com>
2024-03-05 00:43:57 +0530

    Recursion & Backtracking

---
+++ 03. Recursion & Backtracking/01. Permutation - 1.java
@@ -0,0 +1,27 @@
+
+
+//   https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/permutation-i-official/ojquestion
+
+
+class main{
+
+  public static void permutations(int[] boxes, int ci, int ti){
+    
+    if(ci > ti){
+        for(int val : boxes){
+            System.out.print(val + "");
+        }
+        System.out.println();
+        return;
+    }
+
+    for(int i = 0 ; i < boxes.length; i++){
+        if(boxes[i] == 0){
+            boxes[i] = ci;
+            permutations(boxes, ci + 1, ti);
+            boxes[i] = 0;
+        }
+    }
+  }
+
+}
