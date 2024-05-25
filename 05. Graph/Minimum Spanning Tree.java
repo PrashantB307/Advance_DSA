c04f23b840adbaca3993588a1e303de688bab452
PrashantB-307 <pb28.rkt@gmail.com>
2024-05-25 23:40:14 +0530

    Minimum Spanning Tree

--- 05. Graph/Minimum Spanning Tree.java
+++ 05. Graph/Minimum Spanning Tree.java
@@ -5,6 +5,52 @@
 
 import java.util.*;
 class Solution {
+    static class Pair {
+        int wgt;
+        int node;
+
+        Pair(int wgt, int node) {
+            this.wgt = wgt;
+            this.node = node;
+        }
+    }
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((a, b) -> (a.wgt - b.wgt));
+        int[] vis = new int[V];
+        pq.add(new Pair(0 , 0));
+        int sum = 0;
+        
+        while(pq.size() > 0){
+            Pair curr = pq.remove();
+            int wt = curr.wgt;
+            int node = curr.node;
+            
+            if(vis[node] == 1){
+                continue;
+            }
+            vis[node] = 1;
+            sum += wt;
+            
+            for(int i = 0; i < adj.get(node).size(); i++){
+                int nwt = adj.get(node).get(i)[1];
+                int nnode = adj.get(node).get(i)[0];
+                
+                if(vis[nnode] == 0){
+                    pq.add(new Pair(nwt, nnode));
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX====>   OLD Version   <====XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution2 {
 
     static class Pair {
         int wgt;
