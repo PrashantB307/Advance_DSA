4591a16cffb40d2347ae51c06b56969d1b4fe988
PrashantB-307 <pb28.rkt@gmail.com>
2024-05-25 19:29:59 +0530

    Detect Cycle in an Directed Graph

--- 05. Graph/Detect Cycle in an Directed Graph.java
+++ 05. Graph/Detect Cycle in an Directed Graph.java
@@ -45,3 +45,47 @@ class Solution {
         return false;
     }
 }
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=====>   Using Topological Sorting   <====XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution2 {
+  
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] in = new int[V];
+        
+        for(int i = 0; i < V; i++){
+            for(int nbr : adj.get(i)){
+                in[nbr]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < V; i++){
+            if(in[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        int cnt = 0;
+    
+        
+        while(q.size() > 0){
+            int node = q.remove();
+            cnt++;
+            
+            for(int nbr : adj.get(node)){
+                in[nbr]--;
+                if(in[nbr] == 0){
+                    q.add(nbr);
+                }
+            }
+        }
+        
+        if(cnt == V){
+            return false;
+        }
+        
+        return true;
+    }
+}
