d3dbfd8e787ec7142bfad893643ba182e1d32014
PrashantB-307 <pb28.rkt@gmail.com>
2024-06-08 15:58:35 +0530

    Continuous Subarray Sum 523

--- 01. HashMap & HashSet/Continuous Subarray Sum 523.java
+++ 01. HashMap & HashSet/Continuous Subarray Sum 523.java
@@ -30,3 +30,38 @@ class Solution {
         return false;
     }
 }
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=====>   NEW    <=====XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution2 {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        
+        int sum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            sum += nums[i];
+
+            if(nums[i - 1] == 0 && nums[i] == 0){
+                return true;
+            }
+
+            if(sum % k == 0){
+                return true;
+            }
+
+            int j = 0;
+            int temp = sum;
+
+            while ((i - j) > 1 && temp >= k) {
+                temp -= nums[j];
+                j++;
+
+                if(temp % k == 0){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
