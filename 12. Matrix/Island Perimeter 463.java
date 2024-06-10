ef403b5b46a4ee514a5e82ed3e3c6da89319b397
PrashantB-307 <pb28.rkt@gmail.com>
2024-06-10 16:45:26 +0530

    Island Perimeter 463

---
+++ 12. Matrix/Island Perimeter 463.java
@@ -0,0 +1,31 @@
+
+
+//   https://leetcode.com/problems/island-perimeter/
+
+//   https://www.codingninjas.com/studio/problems/find-the-perimeter_2824777
+
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        
+        int p = 0;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    p += 4;
+
+                    if(i > 0 && grid[i - 1][j] == 1){
+                        p -= 2;
+                    }
+
+                    if(j > 0 && grid[i][j - 1] == 1){
+                        p -= 2;
+                    }
+                }
+            }
+        }
+
+        return p;
+    }
+}
