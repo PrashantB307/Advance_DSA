3801ffb256797733b1d9225b2b1c64f76b5f4537
PrashantB-307 <pb28.rkt@gmail.com>
2024-06-20 22:40:41 +0530

    Number of Wonderful Substrings 1915

---
+++ 02. Bit Manipulation/Number of Wonderful Substrings 1915.java
@@ -0,0 +1,24 @@
+
+
+//   https://leetcode.com/problems/number-of-wonderful-substrings/
+
+
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        int count[] = new int[1024];
+        int mask = 0;
+        count[0] = 1;
+        long result = 0;
+
+        for (char c : word.toCharArray()) {
+            mask ^= 1 << (c - 'a');
+            result += count[mask];
+            for (int i = 0; i < 10; i++) {
+                result += count[mask ^ (1 << i)];
+            }
+            count[mask]++;
+        }
+
+        return result;
+    }
+}
