858dd3260f5cfd3ff9028360e0bcdfa516833ccc
PrashantB-307 <pb28.rkt@gmail.com>
2024-06-24 23:11:40 +0530

    Trail of Ones

---
+++ 07. Dynamic Programming/Trail of Ones.java
@@ -0,0 +1,23 @@
+
+
+//   https://www.geeksforgeeks.org/problems/trail-of-ones3242/1
+
+
+class Solution {
+    static int numberOfConsecutiveOnes(int n) {
+        int MOD = 1000000007;
+        int[][] dp = new int[n + 1][3];
+
+        dp[2][0] = 2;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][1] = dp[i - 1][0];
+            dp[i][2] = ((2 * dp[i - 1][2]) % MOD + dp[i - 1][1]) % MOD;
+        }
+
+        return dp[n][2];
+    }
+}
