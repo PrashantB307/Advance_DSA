7afca6576d288eba0b9e527782ce3268e6bcadb9
PrashantB-307 <pb28.rkt@gmail.com>
2024-07-06 22:49:44 +0530

    Divisor Game

---
+++ 13. Mathmetical/Divisor Game.java
@@ -0,0 +1,14 @@
+
+
+//   https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        if(n % 2 == 0){
+            return true;
+        }
+        
+        return false;
+    }
+}
