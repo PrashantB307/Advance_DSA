d2d07bea5ba4ce8b486af2c2618265e1f2c1a1bc
PrashantB-307 <pb28.rkt@gmail.com>
2024-07-06 22:49:22 +0530

    Base Equivalence

---
+++ 13. Mathmetical/Base Equivalence.java
@@ -0,0 +1,21 @@
+
+
+//   https://www.geeksforgeeks.org/problems/base-equivalence1022/1
+
+
+class Solution
+{
+    String baseEquiv(int n, int m)
+    {
+        int i = 2;
+        while (i <= 32) {
+            int digit = (int)(Math.log(n) / Math.log(i)) + 1;
+            if(digit == m){
+                return "Yes";
+            }
+            i++;
+        }
+
+        return "No";
+    }
+}
