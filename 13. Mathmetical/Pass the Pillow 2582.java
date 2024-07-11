1536ca8f129e1a64ef11a4f20cde9590f05dbfe2
PrashantB-307 <pb28.rkt@gmail.com>
2024-07-11 09:24:07 +0530

    Pass the Pillow 2582

---
+++ 13. Mathmetical/Pass the Pillow 2582.java
@@ -0,0 +1,27 @@
+
+
+//   https://leetcode.com/problems/pass-the-pillow/
+
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        
+        int div = time / (n - 1);
+        int rem = time % (n - 1);
+
+        if(rem == 0){
+            if(div % 2 == 0){
+                return 1;
+            }else{
+                return n;
+            }
+        }else{
+            if(div % 2 == 0){
+                return rem + 1;
+            }else{
+                return n - rem;
+            }
+        }
+
+    }
+}
