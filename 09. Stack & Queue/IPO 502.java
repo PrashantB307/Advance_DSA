7421a8dd2cd12f5148cd2f499534ed5a9b58b556
PrashantB-307 <pb28.rkt@gmail.com>
2024-10-11 17:14:47 +0530

    IPO 502

---
+++ 09. Stack & Queue/IPO 502.java
@@ -0,0 +1,35 @@
+
+
+//    https://leetcode.com/problems/ipo/
+ 
+
+import java.util.*;
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int i = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        while (k-- > 0) {
+            while (i < n && arr[i][0] <= w) {
+                pq.offer(arr[i][1]);
+                i++;
+            }
+            if (pq.isEmpty()) {
+                break;
+            }
+            w += pq.poll();
+        }
+        
+        return w;
+    }
+}
