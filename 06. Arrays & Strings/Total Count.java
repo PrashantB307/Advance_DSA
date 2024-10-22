cd145f8626c1bb85bee24ee3b9443d136518ec13
PrashantB-307 <pb28.rkt@gmail.com>
2024-10-22 19:39:49 +0530

    Total Count

---
+++ 06. Arrays & Strings/Total Count.java
@@ -0,0 +1,17 @@
+
+//   https://www.geeksforgeeks.org/problems/total-count2415/1
+
+class Solution {
+    int totalCount(int k, int[] arr) {
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % k == 0) {
+                cnt += arr[i] / k;
+            } else {
+                cnt += arr[i] / k + 1;
+            }
+        }
+
+        return cnt;
+    }
+}
