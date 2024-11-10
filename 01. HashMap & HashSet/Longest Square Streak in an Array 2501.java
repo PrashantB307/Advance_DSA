8681ebe5e691d1427fe08c4eb24ee49695a1b54b
PrashantB-307 <pb28.rkt@gmail.com>
2024-11-10 19:49:50 +0530

    Longest Square Streak in an Array 2501

---
+++ 01. HashMap & HashSet/Longest Square Streak in an Array 2501.java
@@ -0,0 +1,26 @@
+
+
+//   https://leetcode.com/problems/longest-square-streak-in-an-array/
+
+
+import java.util.*;
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(nums);
+        int res = -1;
+
+        for (int num : nums) {
+            int sqrt = (int) Math.sqrt(num);
+
+            if (sqrt * sqrt == num && map.containsKey(sqrt)) {
+                map.put(num, map.get(sqrt) + 1);
+                res = Math.max(res, map.get(num));
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        return res;
+    }
+}
